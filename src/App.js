29c1650c3a0d37a87a767a2ef57a9653335744a9
Muhammad Dawood Umar <dawoodumar720@gmail.com>
2023-04-08 03:50:03 -0700

    counter app added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { incNumber, decNumber } from "./actions/index";
 
 function App() {
+  const myState = useSelector((state) => state.changeTheNumber);
+  const dispath = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Increment and Decrement counter</h1>
+      <div className="container">
+        <button onClick={() => dispath(incNumber())}>+</button>
+        <h2 id="counting">{myState}</h2>
+        <button onClick={() => dispath(decNumber())}>-</button>
+      </div>
     </div>
   );
 }
